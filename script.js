b4b63527b2db1722ab3f6a64a6b0239a4496618e
Deepak Kumar <40654292+deepak-bits@users.noreply.github.com>
2023-07-15 20:06:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+const strongTags = document.querySelectorAll('strong');
+
 function highlight() {
     //Write your code here
-
+	for(let tag of strongTags) {
+		tag.style.color = 'green';
+	}
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+	for(let tag of strongTags) {
+		tag.style.color = 'black';
+	}
     
 }
